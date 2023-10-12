7a1d938a16629130c3f0cf2f33e5f518cfcf5c47
Jangwon Seo <jang079@students.cs.ubc.ca>
2023-10-12 16:32:24 +0900

    editview updating and deleting location implemented

--- mapapp/urls.py
+++ mapapp/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', mapapp.views.main),
     path('adminis/', mapapp.views.index),
     path('add_location/', mapapp.views.add_location, name='add_location'),
+    path('update_location/', mapapp.views.update_location, name='update_location'),
+    path('delete_location/', mapapp.views.delete_location, name='delete_location'),
 ]
--- mapapp/views.py
+++ mapapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import location
 from django.contrib import messages
 import json
+from django.urls import reverse
+from django.template import RequestContext
+
 
 # Create your views here.
 def main(request):
@@ -25,4 +28,19 @@ def add_location(request):
     loc.save()
     return render(request, 'mapapp/map.html', {})
 
-    
+def update_location(request):
+    origin_address = request.POST['originaladdress']
+    new_address = request.POST['address']
+    new_description = request.POST['description']
+    item = location.objects.get(address=origin_address)
+    item.address = new_address
+    item.description = new_description
+    item.save()
+    return render(request, 'mapapp/map.html')
+
+def delete_location(request):
+    origin_address = request.POST['originaladdress']
+    print(origin_address)
+    item = location.objects.get(address=origin_address)
+    item.delete()
+    return render(request, 'mapapp/map.html')
